a918affe8d9de72273602ec841c596c0694997b8
ThibCL <thibault.cabourg@gmail.com>
2020-01-13 00:46:48 +0100

    fix beard error

--- public/js/end.js
+++ public/js/end.js
@@ -3,21 +3,23 @@ function startHistory(history) {
   let hist = JSON.parse(history)
   let keys = Object.keys(hist)
   keys.forEach(function(key, index) {
-    let img = document.getElementById("avimg")
+    if (hist[key] != undefined) {
+      let img = document.getElementById("avimg")
 
-    let p = document.createElement("p")
-    p.className = "line"
-    p.innerHTML = hist[key]["answer"]
+      let p = document.createElement("p")
+      p.className = "line"
+      p.innerHTML = hist[key]["answer"]
 
-    setTimeout(function() {
-      document.getElementById("lines").appendChild(p)
-      img.src = hist[key]["general"]["url"]
-    }, index * 2000)
+      setTimeout(function() {
+        document.getElementById("lines").appendChild(p)
+        img.src = hist[key]["general"]["url"]
+      }, index * 2000)
 
-    setTimeout(function() {
-      p.onmouseover = () => updateImg(hist[key]["alone"]["url"])
-      p.onmouseleave = () => updateImg(hist["skin_tone"]["general"]["url"])
-    }, keys.length * 2000)
+      setTimeout(function() {
+        p.onmouseover = () => updateImg(hist[key]["alone"]["url"])
+        p.onmouseleave = () => updateImg(hist["skin_tone"]["general"]["url"])
+      }, keys.length * 2000)
+    }
   })
 }
 
