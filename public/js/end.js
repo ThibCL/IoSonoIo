6c276a45dec9d4f6f406bacab759808cac11f9dd
ThibCL <thibault.cabourg@gmail.com>
2020-01-15 10:04:23 +0100

    change end earlier

--- public/js/end.js
+++ public/js/end.js
@@ -2,6 +2,7 @@
 function startHistory(history) {
   let hist = JSON.parse(history)
   let keys = Object.keys(hist)
+  let counter = 0
   keys.forEach(function(key, index) {
     if (hist[key]["answer"] != "") {
       let img = document.getElementById("avimg")
@@ -13,12 +14,14 @@ function startHistory(history) {
       setTimeout(function() {
         document.getElementById("lines").appendChild(p)
         img.src = hist[key]["general"]["url"]
-      }, index * 2000)
+      }, counter * 2000)
 
       setTimeout(function() {
         p.onmouseover = () => updateImg(hist[key]["alone"]["url"])
         p.onmouseleave = () => updateImg(hist["skin_tone"]["general"]["url"])
       }, keys.length * 2000)
+
+      counter++
     }
   })
 }
